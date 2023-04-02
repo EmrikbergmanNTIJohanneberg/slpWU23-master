2542b881e710d9c917ca99783790ade458ce7be1
EmrikbergmanNTIJohanneberg <119053225+EmrikbergmanNTIJohanneberg@users.noreply.github.com>
2023-04-02 14:41:50 +0200

    Sidor sidor sidor!

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let layer = document.querySelector(".fade-layer");
 layer.addEventListener("click",showMenu)
 
-let button = document.querySelector(".menu-button");
+let button = document.querySelector(".material-symbols-outlined");
 button.addEventListener("click",showMenu)
 
 function showMenu() {
-    let menu = document.querySelector("nav.menu");
+    let menu = document.querySelector("div.menu");
     menu.classList.toggle("show");
 
     let layer = document.querySelector(".fade-layer");
